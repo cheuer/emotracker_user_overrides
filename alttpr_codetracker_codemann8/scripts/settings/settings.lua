66a40fc5934610b4337ed2898cec01d4292ccfdc
Christopher Heuer <cheuer@gmail.com>
2022-03-19 13:56:39 -0600

    more defaults

--- alttpr_codetracker_codemann8/scripts/settings/settings.lua
+++ alttpr_codetracker_codemann8/scripts/settings/settings.lua
@@ -3,3 +3,5 @@
 ------------------------------------------------------------------
 AUTOTRACKER_ENABLE_AUTOPIN_CURRENT_DUNGEON = false
 LAYOUT_ENABLE_ALTERNATE_DUNGEON_VIEW = true
+LAYOUT_USE_THIN_HORIZONTAL_PANE = false
+LAYOUT_ROOM_SLOT_METHOD = "exist"
